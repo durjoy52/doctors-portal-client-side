c84973791657d7b4a4f594e6538f581fbf8e647f
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-05-16 10:04:36 +0600

    footer set

---
+++ src/pages/Shared/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import footer from '../../assets/images/footer.png';
+const Footer = () => {
+    return (
+        <footer style={{background:`url(${footer})`,backgroundSize:'cover',backgroundPosition:'center',backgroundRepeat:'no-repeat'}} >
+  <div className='footer justify-items-center'>
+  <div>
+    <span className="footer-title">Services</span> 
+    <a className="link link-hover">Emergency Checkup</a>
+    <a className="link link-hover">Monthly Checkup</a>
+    <a className="link link-hover">Weekly Checkup</a>
+    <a className="link link-hover">Deep Checkup</a>
+  </div> 
+  <div>
+    <span className="footer-title">ORAL HEALTH</span> 
+    <a className="link link-hover">Fluoride Treatment</a>
+    <a className="link link-hover">Cavity Filling</a>
+    <a className="link link-hover">Teath Whitening</a>
+  </div> 
+  <div>
+    <span className="footer-title">OUR ADDRESS</span>
+    <a className="link link-hover">New York - 101010 Hudson</a>
+  </div>
+  </div>
+   <div className='my-12 text-center'>
+    <p>{`Copyright © ${new Date().getFullYear()} - All right reserved by ACME Industries Ltd`}</p>
+  </div>
+</footer>
+    );
+};
+
+export default Footer;
